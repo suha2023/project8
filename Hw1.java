06e0619067ff20b3e90a50f60bc6a31f2c9ab01b
suha <150838404+suha2023@users.noreply.github.com>
2024-01-17 22:46:13 +0300

    Add files via upload

---
+++ Hw1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.hw1;
+
+/**
+ *
+ * @author suha9
+ */
+public class Hw1 {
+public void method(){
+    Circle c;
+    System.out.println("What is radus" +c.getRadius());
+    c = new Circle();
+}
+    public static void main(String[] args) {
+        C c=new C(5.0);
+        System.out.println(c.value);
+    }
+}
+class C{
+    int value=2;
+}
